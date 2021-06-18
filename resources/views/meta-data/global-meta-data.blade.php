6c5fe0b9778146b71821f142a67a540077c84104
Jessie Xie <jessie75919@gmail.com>
2021-06-18 12:28:14 +0800

    Chore: modify the meta

--- resources/views/meta-data/global-meta-data.blade.php
+++ resources/views/meta-data/global-meta-data.blade.php
@@ -6,7 +6,7 @@
 <meta name="robots" content="index, follow"/>
 <meta name="googlebot" content="index, follow"/>
 <meta property="og:title" content="圈圈量量"/>
-<meta property="og:description" content="用圈圈體重計記錄每天的身體狀態，隨時掌握身體健康"/>
+<meta property="og:description" content="圈圈・量量看每天的身體狀態，隨時掌握身體健康"/>
 <meta property="og:site_name" content="chan chan creative 圈圈創意"/>
 <meta property="og:type" content="website"/>
 <meta property="og:url" content="https://weight.chanchandev.com/"/>
